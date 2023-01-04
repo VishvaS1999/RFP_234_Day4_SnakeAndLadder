23fcb287dd2e91fd1c5627a3937789b21f4ef20d
VishvaS1999 <gvishva2601@gmail.com>
2023-01-04 17:24:27 +0530

    Welcome Snake And Ladder Game

---
+++ src/com/SnakeAndLadder/WelcomeSnakeAndLadder.java
@@ -0,0 +1,9 @@
+package com.SnakeAndLadder;
+
+public class WelcomeSnakeAndLadder
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome Snake And Ladder Program!..");
+    }
+}
