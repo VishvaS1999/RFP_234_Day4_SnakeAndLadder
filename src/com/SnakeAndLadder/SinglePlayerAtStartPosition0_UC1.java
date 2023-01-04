4965f3d947eff35009b6f8dc0d1625729a4663a1
VishvaS1999 <gvishva2601@gmail.com>
2023-01-04 17:26:12 +0530

    Single Player At Start Position 0

---
+++ src/com/SnakeAndLadder/SinglePlayerAtStartPosition0_UC1.java
@@ -0,0 +1,11 @@
+package com.SnakeAndLadder;
+
+public class SinglePlayerAtStartPosition0_UC1
+{
+    public static void main(String[] args)
+    {
+        int playerPosition=0;
+
+        System.out.println("Single Player at Start Position : " +playerPosition);
+    }
+}
