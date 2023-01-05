bc019aa168220c49b6d48fe992b57f8af669702f
VishvaS1999 <gvishva2601@gmail.com>
2023-01-05 14:25:12 +0530

    Player Exact Winning Till 100

---
+++ src/com/SnakeAndLadder/PlayerExactWinningTill100_UC5.java
@@ -0,0 +1,49 @@
+package com.SnakeAndLadder;
+
+import java.util.Random;
+
+public class PlayerExactWinningTill100_UC5
+{
+    public static void main(String[] args)
+    {
+        Random random = new Random();
+        int singlePlayer = 0;
+        System.out.println("Starting Position of Single Player is :" + singlePlayer);
+
+        while (singlePlayer != 100)
+        {
+            int die = random.nextInt(6) + 1;
+            int a = random.nextInt(3);
+            System.out.println();
+            System.out.println("Player Rolls The Die & Get a Number :" + die);
+            if (a == 1)
+            {
+                singlePlayer =singlePlayer + die;
+                System.out.println("Gets a Ladder & Player Moves ahead By Number Of Position :" + singlePlayer);
+
+                if (singlePlayer > 100)
+                {
+                    singlePlayer = 100;
+                    System.out.println("Player Current Posotion :" + singlePlayer);
+                }
+            }
+            else if (a == 2)
+            {
+                singlePlayer =singlePlayer - die;
+                System.out.println("Bites By Snake & Player Moves Behind By Number Of Position :" + singlePlayer);
+
+                if ( singlePlayer < 0)
+                {
+                    singlePlayer = 0;
+                    System.out.println("Player Current Position :" + singlePlayer);
+                }
+            }
+            else
+            {
+                System.out.println("No Play - Player Stay At Same Position");
+            }
+        }
+        System.out.println();
+        System.out.println("Player Reaches position: " +singlePlayer);
+    }
+}
