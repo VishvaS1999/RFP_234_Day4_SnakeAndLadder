8974e29f0feef044df8a3b52a283de7144c0e521
VishvaS1999 <gvishva2601@gmail.com>
2023-01-19 18:34:58 +0530

    Play with 2 player Snak And Ladder

---
+++ src/com/SnakeAndLadder/PlayWith2Player_UC7.java
@@ -0,0 +1,96 @@
+package com.SnakeAndLadder;
+
+import java.util.Random;
+
+public class PlayWith2Player_UC7
+{
+    static int SnakeLadderPlayer1Player2(int playerPosition, int count)
+    {
+        Random random = new Random();
+        while (playerPosition != 100)
+        {
+            count++;
+            int die = random.nextInt(6) + 1;
+            int a = random.nextInt(3);
+            System.out.println();
+            System.out.println("Player Rolls The Die & Get a Number :" + die);
+            if (a == 1)
+            {
+                playerPosition =playerPosition + die;
+                System.out.println("Gets a Ladder & Player Moves ahead By Number Of Position :" + playerPosition);
+
+                if (playerPosition == 100)
+                    break;
+
+                if (playerPosition > 100)
+                {
+                    playerPosition =playerPosition - die;
+                    System.out.println();
+                    System.out.println("The Number Goes more 100");
+                    System.out.println("Therefore Player Current Position: " + playerPosition);
+                    System.out.println("The Number Required By Player is: " + (100 - playerPosition));
+                }
+                {
+                    die = random.nextInt(6)+1;
+                    playerPosition =playerPosition + die;
+                    System.out.println("Gets Ladder So Playing Again and Moves Ahead by Number of position: "+die);
+                    System.out.println("Player current position: "+playerPosition);
+                }
+            }
+            else if (a == 2)
+            {
+                playerPosition =playerPosition - die;
+                System.out.println("Bites By Snake & Player Moves Behind By Number Of Position :" + playerPosition);
+
+                if (playerPosition < 0)
+                {
+                    playerPosition = 0;
+                    System.out.println("Player Current Position :" + playerPosition);
+                }
+            }
+            else
+            {
+                System.out.println("No Play - Player Stay At Same Position");
+                System.out.println("Player Current Position :" + playerPosition);
+            }
+            if (playerPosition > 100)
+            {
+                playerPosition =playerPosition - die;
+                System.out.println();
+                System.out.println("The Number Goes more 100");
+                System.out.println("Therefore Player Current Position: " + playerPosition);
+                System.out.println("The Number Required By Player is: " + (100 - playerPosition));
+            }
+            System.out.println();
+            System.out.println("Number of times player played: " + count);
+            System.out.println();
+        }
+        return count;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("Starting Position of Single Player is : 0");
+        System.out.println();
+
+        int player1 = SnakeLadderPlayer1Player2(0, 0);
+        {
+            System.out.println("Number of Times player1 played: " +player1);
+        }
+
+        int player2 = SnakeLadderPlayer1Player2(0, 0);
+        {
+            System.out.println("Number of Times player2 Played: "+player2);
+        }
+        if (player1 > player2)
+        {
+            System.out.println("Player2 Won  The Game");
+        }
+        else
+        {
+            System.out.println("Player1 Won The Game");
+        }
+
+
+    }
+}
